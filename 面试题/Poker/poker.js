7bd17666b996500f2bbe3644307a517a1b02299a
澄三彩 <634725355@qq.com>
2021-03-24 18:49:28 +0800

    还没写完

---
+++ 面试题/Poker/poker.js
@@ -0,0 +1,20 @@
+{   
+
+    const fs = require('fs')
+    
+
+    const 
+
+}
+
+{
+    const momo = ['C9D7D9S7D2;HAC5S8D8C10', 'DQSJD8C4DA;H3C9H7D6S2', 'S5HAHJS9DQ;D3S7C6D6S3'];  
+    
+    function init() {
+        
+    }
+
+    function score() {
+
+    }
+}
