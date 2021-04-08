cabb06a99c44a9d7ac8fa42f76406c2c9dac5609
澄三彩 <634725355@qq.com>
2021-04-08 15:45:42 +0800

    添加if判断不让其修改

--- 谷歌插件哒/replaceIp/js/main.js
+++ 谷歌插件哒/replaceIp/js/main.js
@@ -7,8 +7,8 @@
             const aArray = document.querySelectorAll('.content-wrapper a');
             const addCopy = document.querySelector("#http_project_clone");
             console.log("获取所有的a标签", addCopy);
-
-            addCopy.value = addCopy.value.replace(/^http:\/\/gitlab.yanhuamedical.com/,'http://110.185.169.13:90');
+            
+            if (addCopy) addCopy.value = addCopy.value.replace(/^http:\/\/gitlab.yanhuamedical.com/,'http://110.185.169.13:90');
 
             aArray.forEach(p => {
                 if (/^http:\/\/gitlab.yanhuamedical.com/.test(p.href)) p.href = p.href.replace(/^http:\/\/gitlab.yanhuamedical.com/, '');
