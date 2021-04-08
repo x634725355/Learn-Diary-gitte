b002990561e0daa05d3bc4fc8ec0b69d0e2dae29
澄三彩 <634725355@qq.com>
2021-04-08 17:07:19 +0800

    修改插件

--- 谷歌插件哒/replaceIp/js/main.js
+++ 谷歌插件哒/replaceIp/js/main.js
@@ -1,19 +1,28 @@
 
 (function () {
-    document.addEventListener('DOMContentLoaded', () => {
-        console.log('我被执行了!');
 
+    function replaceMethod() {
         setTimeout(() => {
             const aArray = document.querySelectorAll('.content-wrapper a');
             const addCopy = document.querySelector("#http_project_clone");
             console.log("获取所有的a标签", addCopy);
-            
-            if (addCopy) addCopy.value = addCopy.value.replace(/^http:\/\/gitlab.yanhuamedical.com/,'http://110.185.169.13:90');
+
+            if (addCopy) addCopy.value = addCopy.value.replace(/^http:\/\/gitlab.yanhuamedical.com/, 'http://110.185.169.13:90');
 
             aArray.forEach(p => {
                 if (/^http:\/\/gitlab.yanhuamedical.com/.test(p.href)) p.href = p.href.replace(/^http:\/\/gitlab.yanhuamedical.com/, '');
             });
         }, 500);
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+        console.log('我被执行了!');
+
+        console.log('路由', window.location);
+
+        const $location = new Proxy(window.location, )
+
+        replaceMethod();
 
     });
 }())
